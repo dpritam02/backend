bdf6ddae064df3558059b6811deb5555084a3788
dpritam02 <daspritam161202@gmail.com>
2024-11-18 19:01:08 +0530

    models are ready

--- Readme.md
+++ Readme.md
@@ -2,4 +2,3 @@
 
 This contain series of backend with javascript (mentor: Chai aur code)
 - [Model](https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj)
-
---
+++ src/models/user.model.js
@@ -0,0 +1,95 @@
+import mongoose from "mongoose";
+import { jwt } from "jsonwebtoken";
+import {bcrypt} from "bcrypt";
+
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true, 
+            index: true //for enabling searching and filtering in any database
+        }, 
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true, 
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true, 
+            index: true
+        },
+        avatar: {
+            type: String, // cloudinary url is used for avatar
+            required: true,
+        },
+        coverImage: {
+            type: String, // cloudinary url is used for cover image
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
+
+    },
+    {
+        timestamps: true //to get the created at and updated at
+    }
+)
+
+//logic to encrypt pasword // pre hook middleware is used for encryption/decryption
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next(); // check if the password is modified
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    //used to check the password is correct
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = async function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = async function(refreshToken) {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
